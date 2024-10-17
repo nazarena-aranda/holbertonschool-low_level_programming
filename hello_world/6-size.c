1a54ff28c8795ce21a2fe0fdb89ab3e61d32db0f
Nazarena Aranda <nazarenaaranda15@gmail.com>
2024-10-17 21:20:48 +0000

    print size of type

---
+++ hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - function entry point
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	char charType;
+	int intType;
+	long int longintType;
+	long long int longlongintType;
+	float floatType;
+
+	printf("Size of char: %zu byte\n", sizeof(charType));
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of a long int: %zu bytes\n", sizeof(longintType));
+	printf("Size of a long long int: %zu bytes\n", sizeof(longlongintType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	return (0);
+}
