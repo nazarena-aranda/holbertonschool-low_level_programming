611c5c455128c104c1feacf4d5e1abb3d922e200
Nazarena Aranda <nazarenaaranda15.com>
2024-10-22 16:30:23 +0000

    checked letter

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <ctype.h>
 /**
- * _islower(int c) - find cases lowercase
+ * _islower  - find cases lowercase
+ * @c: the letter to be checked
  * Return: 1 if c is lowercase 0 otherwise
  */
 
-int _islower(int c) {
-
+int _islower(int c)
+{
 	if (islower(c))
 		return (1);
 	else
