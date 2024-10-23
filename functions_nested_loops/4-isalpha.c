f8e1ceaae1e58848fcaae62bdab1719d874f171a
Nazarena Aranda <nazarenaaranda15@gmail.com>
2024-10-23 17:43:48 +0000

    is alpha?

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
-#include "main.h"
 #include <ctype.h>
+#include "main.h"
 /**
- * _islower  - checks for alphabetic character
+ * _isalpha  - checks for alphabetic character
  * @c: thing to check
  * Return: 1 if c is a letter, lowercase or uppercase 0 otherwise
  */
 
 int _isalpha(int c)
 {
-	if (isalpha(c))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
 	else
 		return (0);
