0d2426bf1d437b0cf3f39829f2fa341605adb8cb
Nazarena Aranda <nazarenaaranda15@gmail.com>
2024-10-25 20:02:27 +0000

    print every minute of the day

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -10,18 +10,21 @@ void jack_bauer(void)
 
 	for (a = 0; a <= 2; a++)
 	{
-		for (b = 0; b <= 3; b++)
+		for (b = 0; b <= 9; b++)
 		{
-			for (c = 0; c <= 5; c++)
+			if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 			{
-				for (d = 0; d <= 9; d++)
+				for (c = 0; c <= 5; c++)
 				{
-					_putchar(a + 48);
-					_putchar(b + 48);
-					_putchar(':');
-					_putchar(c + 48);
-					_putchar(d + 48);
-					_putchar('\n');
+					for (d = 0; d <= 9; d++)
+					{
+						_putchar(a + 48);
+						_putchar(b + 48);
+						_putchar(':');
+						_putchar(c + 48);
+						_putchar(d + 48);
+						_putchar('\n');
+					}	
 				}
 			}
 		}
