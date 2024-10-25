2b6da0744edf82fb901dd965f93c551769dce181
Nazarena Aranda <nazarenaaranda15@gmail.com>
2024-10-25 23:37:16 +0000

    print numbers from n to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,28 +7,30 @@
 
 void print_to_98(int n)
 {
-	if (n <= 98)
+	for (n = n; n <= 98; n++)
 	{
-		for (n = n; n <= 98; n++)
+		int i = n;
+
+		if (i < 0)
 		{
-			if (n != 98)
-			{
-				if (n > 10)
-				{
-					_putchar(n / 10);
-					_putchar(n % 10);
-					_putchar(',');
-					_putchar(' ');
-				}
-				else
-				{
-					_putchar(n + 48);
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
-			_putchar(n + 48);
+			_putchar('-');
+			i = i * -1;
 		}
+
+		if (i >= 10)
+		{
+			_putchar(i / 10 + 48);
+			_putchar(i % 10 + 48);
+		}
+		else
+		{
+			_putchar(i + 48);
+		}
+		if(n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}	
 	}
 	_putchar('\n');
 }
