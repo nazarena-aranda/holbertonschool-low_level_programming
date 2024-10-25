44b835beca17b87be67d11f8d6126fc9fae232ef
Nazarena Aranda <nazarenaaranda15@gmail.com>
2024-10-25 19:50:15 +0000

    print every minute of the day

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 3; b++)
+		{
+			for (c = 0; c <= 5; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					_putchar(a + 48);
+					_putchar(b + 48);
+					_putchar(':');
+					_putchar(c + 48);
+					_putchar(d + 48);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
