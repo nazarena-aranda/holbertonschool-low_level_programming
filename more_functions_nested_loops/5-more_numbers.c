739e861410cdd69ddb23aa71f32f66e65c309dcb
Nazarena Aranda <nazarenaaranda15@gmail.com>
2024-10-28 18:46:35 +0000

    more numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * more_numbers - function that prints 10 times the numbers, from 0 to 14
+ * Return: 0
+ */
+
+
+void more_numbers(void)
+{
+	int f, c;
+
+	for (f = 0; f < 10; f++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c < 10)
+				_putchar(c + 48);
+			else if (c >= 10)
+			{
+
+				_putchar((c / 10) + 48);
+				_putchar((c % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
