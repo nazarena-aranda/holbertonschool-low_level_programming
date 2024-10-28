7c885938e6f1e14011098273dde27d8432948c9a
Nazarena Aranda <nazarenaaranda15@gmail.com>
2024-10-28 21:36:07 +0000

    print square

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_square - prints a square
+ * @size: size of the square
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+				_putchar ('#');
+		_putchar ('\n');
+		}
+	}
+}
