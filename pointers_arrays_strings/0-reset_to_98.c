d2fa5af8b9543b5062c0da02ff02297498fa4993
Nazarena Aranda <nazarenaaranda15@gmail.com>
2024-10-29 17:59:58 +0000

    delete executable

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that tupdates the value it points to to 98
+ * Return: 0;
+ * @n: pointer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+readme to this project
