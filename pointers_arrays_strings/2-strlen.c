f2852eaa51d0f2e67cdc396161c671b71a5f3e63
Nazarena Aranda <nazarenaaranda15@gmail.com>
2024-10-29 19:25:39 +0000

    count of the size of pointer

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen -  function that returns the length of a string
+ * @s: pointer to str
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		
+	}
+	return (i);
+}
