3156bd6a3667e43a1e6ccd640d081be4d4209c36
Nazarena Aranda <nazarenaaranda@gmail.com>
2024-11-01 19:23:44 +0000

    rev string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,5 +7,22 @@
 
 void rev_string(char *s)
 {
-	print_rev(s);
+	char *end = s;
+	char *start = s;
+	char temp;
+
+	while (*end != '\0')
+		end++;
+	end--;
+
+	while (start < end)
+	{
+		temp = *start;
+		*start = *end;
+		*end = temp;
+
+		start++;
+		end--;
+	}
+
 }
