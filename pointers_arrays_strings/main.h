0b486395521997904e67b1ce75b72f24ad9f50c3
Nazarena Aranda <nazarenaaranda15@gmail.com>
2024-11-02 10:19:12 +0000

    print array

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -9,4 +10,5 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
 #endif
