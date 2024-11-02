da63e14fd4614e5e888aa8c0c271a32efec8195f
Nazarena Aranda <nazarenaaranda15@gmail.com>
2024-11-02 09:22:49 +0000

    half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts_half - function that prints half of a string
+ * @str: string
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int i, mitad, c;
+
+	while (str[i] != '\0')
+		i++;
+
+	if (i % 2 == 0)
+	{
+		mitad = i / 2;
+	}
+	else
+	{
+		mitad = ((i - 1) / 2 + 1);
+	}
+	for (c = mitad; c < i; c++)
+		_putchar(str[c]);
+	
+	_putchar('\n');
+}
