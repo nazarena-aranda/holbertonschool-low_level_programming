e7280e776ab7487a8c8fb0a2c70645a0c16321d8
Nazarena Aranda <nazarenaaranda15@gmail.com>
2024-11-03 00:03:02 +0000

    0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *_strcat - function appends the src string to the dest string
+ * @dest: end
+ * @src: start
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *i = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (i);	
+}
