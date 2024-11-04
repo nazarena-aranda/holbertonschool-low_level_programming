60c555a9289f4d41e95e9a711658d99429ea082d
Nazarena Aranda <nazarenaaranda15@gmail.com>
2024-11-04 02:38:37 +0000

    copie to string

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcmp - compare two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: one string
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
