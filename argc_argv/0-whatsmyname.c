2ea53e720fd651f21ea3570b0a953f81a8b8bb46
Nazarena Aranda <nazarenaaranda06@gmail.com>
2024-11-08 23:34:09 +0000

    argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - print the name of the files
+ * @argc: num of arguments
+ * @argv: num of arguments
+ * Return: 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+int _putchar(char c);
+int main(int argc __attribute__((unused)), char *argv[]);
+#endif
