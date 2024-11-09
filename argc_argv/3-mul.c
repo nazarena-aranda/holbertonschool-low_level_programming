7b4a958a11e26c8d39912afd5fe3c975e98a0386
Nazarena Aranda <nazarenaaranda06@gmail.com>
2024-11-09 01:01:25 +0000

    argv again

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -9,16 +9,16 @@
 
 int main(int argc, char *argv[])
 {
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-
-	if (argc < 2)
+	if (argc != 3)
 	{
 		printf("Error\n");
-		return (-1);
+		return (1);
 	}
 	else
 	{
+		int num1 = atoi(argv[1]);
+		int num2 = atoi(argv[2]);
+
 		printf("%d\n", num1 * num2);
 	}
 	return (0);
