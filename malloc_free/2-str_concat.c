7f6f6a75dfc5194142c148c6013d351e97a98403
Nazarena Aranda <nazarenaaranda06@gmail.com>
2024-11-13 17:24:15 +0000

    concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * str_concat - 
- * 
- *
+ * str_concat - concat two string
+ * @s1: pointer to string 1
+ * @s2: pointer to string 2
+ * Return: s1 + s2
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, j;
+	int len1, len2, i, j;
 	char *p;
 
 	if (s1 == NULL)
@@ -19,18 +20,31 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 	}
 
-	for (i = 0; s1[i] != '\0'; i++);
-	for (j = 0; s2[j] != '\0'; j++);
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+	{
+	}
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+	{
+	}
 
-	p = (char *)malloc((i + j + 1) * sizeof(char));
+	p = (char *)malloc((len1 + len2 + 1) * sizeof(char));
 
 	if (p == NULL)
 	{
 		return (NULL);
 	}
 
-	for (k = 0; k < i; k++)
+	for (i = 0; i < len1; i++)
+	{
+		p[i] = s1[i];
+	}
+
+	for (j = 0; j < len2; j++)
+	{
+		p[i + j] = s2[j];
+	}
+
+	p[i + j] = '\0';
 
 	return (p);
-
 }
