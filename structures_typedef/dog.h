0d0894957fd0cf769533433dde4c77adac6aa2a1
Nazarena Aranda <nazarenaaranda06@gmail.com>
2024-11-15 13:15:06 +0000

    my first struct

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - my first struct
+ * @name: name of the dog
+ * @age: age from the dog
+ * @owner: owner of the dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_;
+
+#endif
