665f0e36220c9736bbe10b279ed5b79c15882b03
Nazarena Aranda <nazarenaaranda06@gmail.com>
2024-11-19 03:05:41 +0000

    print name with call function pointer

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,8 +1,9 @@
 #include "function_pointers.h"
 /**
- *
- *
- *
+ * print_name - print name use function
+ * @*name: name to print
+ * @f: pointer to function
+ * Return: 0
  */
 
 void print_name(char *name, void (*f)(char *))
