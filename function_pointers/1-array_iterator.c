ba8357d895b854435d5ea8cb3758f8398078f81b
Nazarena Aranda <nazarenaaranda06@gmail.com>
2024-11-19 03:29:19 +0000

    print array

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,8 +1,10 @@
 #include "function_pointers.h"
 /**
- *
- *
- *
+ * array_iterator - executes given as a parameter on each element of an array
+ * @array: array of elements
+ * @size: size of array
+ * @action: action that elements
+ * Return: 0
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
